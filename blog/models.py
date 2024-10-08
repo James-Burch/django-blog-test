e1761cbf4c0b4351a008a85b31ee317486cfdde1
James Burch <burchjames6@gmail.com>
2024-10-08 19:14:36 +0000

    Add comments

--- blog/models.py
+++ blog/models.py
@@ -16,28 +16,13 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-class Event(models.Model):
-    event_name = models.CharField(max_length=200, unique=True)
-    location = models.CharField(max_length=200)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.event_name
-
-
-class Ticket(models.Model):
-    ticket_holder = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="users_tickets"
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
     )
-    date_issued = models.DateTimeField(auto_now_add=True)
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
-        related_name="event_tickets"
-    )
-
-
-    def __str__(self):
-        return f"Ticket for {self.ticket_holder}"
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)  
